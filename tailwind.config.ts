55b40d1278fb618ee260040d8e089a81e2519f92
delmac <delalitengue.dev@gmail.com>
2024-12-23 23:26:41 +0000

    styles(tailwind.config): add css variable for themes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,10 +7,89 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    fontFamily: {
+      "inter": ["Inter", "sans-serif"],
+    },
     extend: {
+      screens: {
+        "xs": "480px"
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          50: "hsl(var(--primary-50))",
+          100: "hsl(var(--primary-100))",
+          200: "hsl(var(--primary-200))",
+          300: "hsl(var(--primary-300))",
+          400: "hsl(var(--primary-400))",
+          500: "hsl(var(--primary-500))",
+          600: "hsl(var(--primary-600))",
+          700: "hsl(var(--primary-700))",
+          800: "hsl(var(--primary-800))",
+          900: "hsl(var(--primary-900))",
+          950: "hsl(var(--primary-950))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          50: "hsl(var(--secondary-50))",
+          100: "hsl(var(--secondary-100))",
+          200: "hsl(var(--secondary-200))",
+          300: "hsl(var(--secondary-300))",
+          400: "hsl(var(--secondary-400))",
+          500: "hsl(var(--secondary-500))",
+          600: "hsl(var(--secondary-600))",
+          700: "hsl(var(--secondary-700))",
+          800: "hsl(var(--secondary-800))",
+          900: "hsl(var(--secondary-900))",
+          950: "hsl(var(--secondary-950))",
+        },
+        danger: {
+          50: "hsl(var(--danger-50))",
+          100: "hsl(var(--danger-100))",
+          200: "hsl(var(--danger-200))",
+          300: "hsl(var(--danger-300))",
+          400: "hsl(var(--danger-400))",
+          500: "hsl(var(--danger-500))",
+          600: "hsl(var(--danger-600))",
+          700: "hsl(var(--danger-700))",
+          800: "hsl(var(--danger-800))",
+          900: "hsl(var(--danger-900))",
+          950: "hsl(var(--danger-950))",
+        },
+        success: {
+          50: "hsl(var(--success-50))",
+          100: "hsl(var(--success-100))",
+          200: "hsl(var(--success-200))",
+          300: "hsl(var(--success-300))",
+          400: "hsl(var(--success-400))",
+          500: "hsl(var(--success-500))",
+          600: "hsl(var(--success-600))",
+          700: "hsl(var(--success-700))",
+          800: "hsl(var(--success-800))",
+          900: "hsl(var(--success-900))",
+          950: "hsl(var(--success-950))",
+        },
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        }
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
