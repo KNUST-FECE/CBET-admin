b9e1d3012d96e7f625d76d80b01569627cf9e439
delmac <delalitengue.dev@gmail.com>
2024-12-27 20:58:53 +0000

    chore: remove conn helper function from utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,10 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
-import { Db } from "mongodb";
+import { Db, MongoClient, ObjectId } from "mongodb";
 import client from "./db";
 
+// @ts-expect-error read only property is not assignable
+Symbol.asyncDispose ??= Symbol("Symbol.asyncDispose")
 
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs))
@@ -20,11 +22,57 @@ export const baseUrl = () => {
     
 };
 
-export async function conn<T>(callback: (db: Db, isLocal: boolean) => Promise<T>): Promise<T> {
-    await client.connect();
+/** @internal */
+export function _id(hex?: string) {
+    if (hex?.length !== 24) return new ObjectId()
+    return new ObjectId(hex)
+}  
 
-    const db = client.db("cbet-platform");
-    const isLocal = process.env.NODE_ENV === 'development';
-
-    return await callback(db, isLocal) as T;
+export const format = {
+    /** Takes a MongoDB object and returns a plain old JavaScript object */
+    from<T = Record<string, unknown>>(object: Record<string, any>): T {
+      const newObject: Record<string, unknown> = {}
+      for (const key in object) {
+        const value = object[key]
+        if (key === "_id") {
+          newObject.id = value.toHexString()
+        } else if (key === "userId") {
+          newObject[key] = value.toHexString()
+        } else {
+          newObject[key] = value
+        }
+      }
+      return newObject as T
+    },
+    /** Takes a plain old JavaScript object and turns it into a MongoDB object */
+    to<T = Record<string, unknown>>(object: Record<string, any>) {
+      const newObject: Record<string, unknown> = {
+        _id: _id(object.id),
+      }
+      for (const key in object) {
+        const value = object[key]
+        if (key === "userId") newObject[key] = _id(value)
+        else if (key === "id") continue
+        else newObject[key] = value
+      }
+      return newObject as T & { _id: ObjectId }
+    },
 };
+
+export const getDb = async (onClose?: (client: MongoClient) => Promise<void>) => {
+    const _db = client.db("cbet-platform");
+
+    return {
+      M: _db.collection("members"),
+      U: _db.collection("users"),
+      RC: _db.collection("resources"),
+      B: _db.collection("blogs"),
+      S: _db.collection("shops"),
+      P: _db.collection("products"),
+      RT: _db.collection("reports"),
+      RO: _db.collection("roles"),
+      [Symbol.asyncDispose]: async () => {
+        await onClose?.(client)
+      },
+    }
+}
